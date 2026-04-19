3ed4cd7a83f27ff6ab91c09572058a6e477713d4
Fika Haliza <2511500047@mahasiswa.atmaluhur.ac.id>
2026-04-20 01:40:41 +0700

    login.php

--- login.php
+++ login.php
@@ -1,71 +1,95 @@
 <?php
-session_start();
-include "config/koneksi.php";
-
-if(isset($_POST['login'])){
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $query = mysqli_query($conn,"SELECT * FROM admin WHERE username='$username' AND password='$password'");
-    $cek = mysqli_num_rows($query);
-
-    if($cek > 0){
-        $_SESSION['username']=$username;
-        header("location:starter.php");
-    }else{
-        echo "<script>alert('Username atau Password salah');</script>";
-    }
-}
+    include "config/koneksi.php";
+    session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta churset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>
-        <link rel="stylesheet" href="Style.css">
-        <style>
-         body {
-            background-image: url('Background-01.png');
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-attachment: fixed;
-        }
-</style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="login">
-                <form method="POST">
-                    <h1>Login</h1>
-                    <hr>
-                    <p>Work From Office</p>
-                    <label>Username</label>
-                    <input type="text" name="username" placeholder="Masukkan Username">
-                    <label>Password</label>
-                    <input type="password" name="password" placeholder="Password">
-                    <button type="submit" name="login">Login</button>
-                    <p>
-                        <a href="#">Forgot Password</a>
-                    </p>
-                    <div class="social-icons">
-                        <a href="#" class="icon google" title="Login with Google">
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" alt="Google" style="width:24px;height:24px;">
-                        </a>
-                        <a href="#" class="icon facebook" title="Login with Facebook">
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/facebook/facebook-original.svg" alt="Facebook" style="width:24px;height:24px;">
-                        </a>
-                        <a href="#" class="icon twitter" title="Login with Twitter">
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" alt="Twitter" style="width:24px;height:24px;">
-                        </a>
-                    </div>
-                </form>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b>Admin</b>LTE</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="#" method="post">
+        <div class="input-group mb-3">
+          <input type="text" name="Username" id="Username" class="form-control" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
             </div>
-            <div class="right">
-                <img src="char.png" alt="">
-            </div>
+          </div>
         </div>
-    </body></html>
+        <div class="input-group mb-3">
+          <input type="password" name="Password" id="Password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+
+          <!-- /.col -->
+          <div class="col-12">
+            <input type="submit" name="login" value="Login" class="btn btn-primary btn-block">
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
+<?php
+    if(isset($_POST['Username']))   {
+        $Username = $_POST['Username'];
+        $Password = $_POST['Password'];
+
+        if(empty($Username) || empty($Password))  {
+            echo "Data Tidak Boleh kosong";
+        } else {
+            $userquery = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM 
+              admin WHERE Username ='$Username' AND Password = '$Password' "));
+            if($userquery) {
+              $_SESSION['level'] = 'admin';
+              $_SESSION['Username'] = $Username;
+        header("location:index.php");
+            } else {
+          echo  '<div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert"
+                  aria-hidden="true">x</button>
+                <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+                Login gagal
+              </div>';
+                }
+          }
+    }
+?>
