719e2e0a45c0212e2e79e77c3063d1b557d1aeb4
Fika Haliza <2511500047@mahasiswa.atmaluhur.ac.id>
2026-04-20 01:48:54 +0700

    login.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_mahasiswa";
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "db_mahasiswa";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+    $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
+    if(mysqli_connect_error()){
+        echo "gagal melakukan koneksi ke database :" . mysqli_connect_error(
+            );
+    }
 ?>
